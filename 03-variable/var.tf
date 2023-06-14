c7b3b3e35711d6131890d6a866da1dede9adf2f6
Sangamesh30 <sangamesh.3094@gmail.com>
2023-06-14 13:24:13 +0530

    playbook

--- 03-variable/var.tf
+++ 03-variable/var.tf
@@ -17,3 +17,48 @@ output "sample1" {
 output "sample-ext" {
   value = "value of sample - ${var.sample}"
 }
+
+#plain
+
+variable "devops" {
+  default = "training"
+}
+
+#list
+variable "devops1" {
+  default = [
+    "devops",
+    "aws",
+    "python"
+  ]
+}
+#map
+
+variable "devopsaws" {
+  default = {
+    devops = {
+      name = "devops",
+      time = "2am"
+      dur = "daily"
+    }
+    aws = {
+      name = "aws",
+      time = "3am",
+      dur = "daily"
+    }
+  }
+}
+
+output "devops" {
+  value = "var.devops"
+}
+
+
+output "devops1" {
+  value = "var.devops1"
+}
+
+
+output "devopsaws" {
+  value = "var.devopsaws"
+}
