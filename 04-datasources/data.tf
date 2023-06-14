be21ff833606779a5af035058b2c7d6bb34456dd
Sangamesh30 <sangamesh.3094@gmail.com>
2023-06-14 19:53:47 +0530

    playbook

---
+++ 04-datasources/data.tf
@@ -0,0 +1,13 @@
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.medium"
+  availability_zone = "us-east-1a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
+}
+
+output "price" {
+  value = data.aws_ec2_spot_price.example.spot_price
+}
