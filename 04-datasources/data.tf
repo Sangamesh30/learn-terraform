336fd7db1c3cbbacdb3eb31f6a93501f0aa44c9c
Sangamesh30 <sangamesh.3094@gmail.com>
2023-06-14 20:14:50 +0530

    playbook

--- 04-datasources/data.tf
+++ 04-datasources/data.tf
@@ -24,6 +24,7 @@ data "aws_ami" "example" {
   executable_users = ["amazon"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
+  owners           = []
 }
 
 output "ami" {
